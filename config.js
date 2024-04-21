569849611fe3aaebdb01e7c957c20e8ade2088a9
Gobinath B <princegopinath42@gmail.com>
2024-04-22 00:11:15 +0530

    refactor

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 var admin = require("firebase-admin");
 
-var serviceAccount = require("./data-hari-671e6-firebase-adminsdk-hpppk-622dec44f4.json"); //add fire base project
+var serviceAccount = require("./sdk.json"); //add fire base project
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
